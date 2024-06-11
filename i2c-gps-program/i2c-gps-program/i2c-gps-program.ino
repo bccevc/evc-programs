579a8d9f9c37364922f8fba8e5a063646c010cf7
bccevc <bccevc@gmail.com>
2024-06-11 10:53:16 -0400

    Format code

--- i2c-gps-program/i2c-gps-program/i2c-gps-program.ino
+++ i2c-gps-program/i2c-gps-program/i2c-gps-program.ino
@@ -26,10 +26,10 @@
     changed values to RXPIN = 2 and TXPIN = to correspond with
     hardware v14+. Hardware v13 used RXPIN = 3 and TXPIN = 2.
   
-*/ 
+*/
 
-// In order for this sketch to work, you will need to download 
-// TinyGPS library from arduiniana.org and put them 
+// In order for this sketch to work, you will need to download
+// TinyGPS library from arduiniana.org and put them
 // into the hardware->libraries folder in your ardiuno directory.
 #include <Wire.h>
 #include <SerLCD.h>
@@ -41,125 +41,122 @@
 
 SerLCD lcd;
 //-SerLCD OpenLCD;
-//lcd.begin(Wire); 
+//lcd.begin(Wire);
 
-// Define which pins you will use on the Arduino to communicate with your 
-// GPS. In this case, the GPS module's TX pin will connect to the 
+// Define which pins you will use on the Arduino to communicate with your
+// GPS. In this case, the GPS module's TX pin will connect to the
 // Arduino's RXPIN which is pin 3.
 #define RXPIN 4
 #define TXPIN 5
 //Set this value equal to the baud rate of your GPS
 #define GPSBAUD 4800
-const int chipSelect = 9; //for SD card
+const int chipSelect = 9;  //for SD card
 // Create an instance of the TinyGPS object
 TinyGPS gps;
 // Initialize the NewSoftSerial library to the pins you defined above
 SoftwareSerial uart_gps(RXPIN, TXPIN);
-//-SoftwareSerial OpenLCD(6,7); // pin 7 = TX, pin 6 = RX (unused)  
+//-SoftwareSerial OpenLCD(6,7); // pin 7 = TX, pin 6 = RX (unused)
 
-// This is where you declare prototypes for the functions that will be 
+// This is where you declare prototypes for the functions that will be
 // using the TinyGPS library.
 void getgps(TinyGPS &gps);
 
-// In the setup function, you need to initialize two serial ports; the 
-// standard hardware serial port (Serial()) to communicate with your 
-// terminal program an another serial port (NewSoftSerial()) for your 
+// In the setup function, you need to initialize two serial ports; the
+// standard hardware serial port (Serial()) to communicate with your
+// terminal program an another serial port (NewSoftSerial()) for your
 // GPS.
-void setup()
-{//start setup
-  // This is the serial rate for your terminal program. It must be this 
-  // fast because we need to print everything before a new sentence 
-  // comes in. If you slow it down, the messages might not be valid and 
+void setup() {
+  // This is the serial rate for your terminal program. It must be this
+  // fast because we need to print everything before a new sentence
+  // comes in. If you slow it down, the messages might not be valid and
   // you will likely get checksum errors.
 
-{
-  //code for SD
- Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
-  Serial.print("Initializing SD card...");
-  // make sure that the default chip select pin is set to
-  // output, even if you don't use it:
-  pinMode(chipSelect, OUTPUT);
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return;
-  }
-  
-  Serial.println("card initialized.");
-}
-//Sd ends here setup
-{
-  Serial.begin(115200);
-  Wire.begin();
-  lcd.begin(Wire);
-
-  Wire.setClock(400000); //Optional - set I2C SCL to High Speed Mode of 400kHz
- //Code for SD card
- 
-  lcd.clear();
-  lcd.cursor(); //Turn on the underline cursor
- //lcd.print("Watch the cursor!");
-  
- //- OpenLCD.begin(9600); // set up serial port for 9600 baud  BobR
-  delay(500); // wait for display to boot up   BobR
-  
-  //Sets baud rate of your GPS
-  uart_gps.begin(GPSBAUD);
-  
-  Serial.println("");
-  Serial.println("GPS Shield QuickStart Example Sketch v12");
-  Serial.println("       ...waiting for lock...           ");
-  Serial.println("");
-}
-}
-// This is the main loop of the code. All it does is check for data on 
-// the RX pin of the ardiuno, makes sure the data is valid NMEA sentences, 
-// then jumps to the getgps() function.
-void loop()
-{
-  while(uart_gps.available())     // While there is data on the RX pin...
   {
-      int c = uart_gps.read();    // load the data into a variable...
-      if(gps.encode(c))      // if there is a new valid sentence...
-      {
-        Serial.println("       ...HERE IT COMES...           ");  //BobR
-        getgps(gps);         // then grab the data.
-      }
+    //code for SD
+    Serial.begin(9600);
+    while (!Serial) {
+      ;  // wait for serial port to connect. Needed for Leonardo only
+    }
+
+    Serial.print("Initializing SD card...");
+    // make sure that the default chip select pin is set to
+    // output, even if you don't use it:
+    pinMode(chipSelect, OUTPUT);
+
+    // see if the card is present and can be initialized:
+    if (!SD.begin(chipSelect)) {
+      Serial.println("Card failed, or not present");
+      // don't do anything more:
+      return;
+    }
+
+    Serial.println("card initialized.");
+  }
+  //Sd ends here setup
+  {
+    Serial.begin(115200);
+    Wire.begin();
+    lcd.begin(Wire);
+
+    Wire.setClock(400000);  //Optional - set I2C SCL to High Speed Mode of 400kHz
+                            //Code for SD card
+
+    lcd.clear();
+    lcd.cursor();  //Turn on the underline cursor
+                   //lcd.print("Watch the cursor!");
+
+    //- OpenLCD.begin(9600); // set up serial port for 9600 baud  BobR
+    delay(500);  // wait for display to boot up   BobR
+
+    //Sets baud rate of your GPS
+    uart_gps.begin(GPSBAUD);
+
+    Serial.println("");
+    Serial.println("GPS Shield QuickStart Example Sketch v12");
+    Serial.println("       ...waiting for lock...           ");
+    Serial.println("");
+  }
+}
+// This is the main loop of the code. All it does is check for data on
+// the RX pin of the ardiuno, makes sure the data is valid NMEA sentences,
+// then jumps to the getgps() function.
+void loop() {
+  while (uart_gps.available())  // While there is data on the RX pin...
+  {
+    int c = uart_gps.read();  // load the data into a variable...
+    if (gps.encode(c))        // if there is a new valid sentence...
+    {
+      Serial.println("       ...HERE IT COMES...           ");  //BobR
+      getgps(gps);                                              // then grab the data.
+    }
   }
 }
 
 // The getgps function will get and print the values we want.
-void getgps(TinyGPS &gps)
-{
-  // To get all of the data into varialbes that you can use in your code, 
-  // all you need to do is define variables and query the object for the 
-  // data. To see the complete list of functions see keywords.txt file in 
+void getgps(TinyGPS &gps) {
+  // To get all of the data into varialbes that you can use in your code,
+  // all you need to do is define variables and query the object for the
+  // data. To see the complete list of functions see keywords.txt file in
   // the TinyGPS and NewSoftSerial libs.
-  
+
   // Define the variables that will be used
   float latitude, longitude;
   // Then call this function
   gps.f_get_position(&latitude, &longitude);
   // You can now print variables latitude and longitude
-  Serial.print("Lat/Long: "); 
-  Serial.print(latitude,5); 
-  Serial.print(", "); 
-  Serial.println(longitude,5);
+  Serial.print("Lat/Long: ");
+  Serial.print(latitude, 5);
+  Serial.print(", ");
+  Serial.println(longitude, 5);
 
-    // make a string for assembling the data to log://made forSD
+  // make a string for assembling the data to log://made forSD
   String dataString = "";
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   // this opens the file and appends to the end of file
   // if the file does not exist, this will create a new file.
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
-if (dataFile)   {  
+  if (dataFile) {
     int timeStamp = millis();
     //write to uSD card
     dataFile.print(timeStamp);
@@ -168,93 +165,114 @@ if (dataFile)   {
     //output also on Serial monitor for debugging
     Serial.print(timeStamp);
     Serial.print(",");
-    
-    dataFile.print("Lat/Long: "); 
-    dataFile.print(latitude,5); 
-   dataFile.print(", ");
-   dataFile.print(longitude,5);
 
-   int year;
-  byte month, day, hour, minute, second, hundredths;
-  gps.crack_datetime(&year,&month,&day,&hour,&minute,&second,&hundredths);
-  // Print data and time
-  dataFile.print("Date: "); dataFile.print(month, DEC);dataFile.print("/"); 
-  dataFile.print(day, DEC);dataFile.print("/"); dataFile.print(year);
-  int echour = hour - 5;
-  dataFile.print("  Time: "); dataFile.print(echour, DEC); dataFile.print(":"); 
-  dataFile.print(minute, DEC); dataFile.print(":"); dataFile.print(second, DEC); 
-  dataFile.print("."); dataFile.println(hundredths, DEC);
+    dataFile.print("Lat/Long: ");
+    dataFile.print(latitude, 5);
+    dataFile.print(", ");
+    dataFile.print(longitude, 5);
 
-   // Here you can print the altitude and course values directly since 
-  // there is only one value for the function
-  dataFile.print("Altitude (meters): "); dataFile.println(gps.f_altitude());  
-  // Same goes for course
-  dataFile.print("Course (degrees): "); dataFile.println(gps.f_course()); 
-  // And same goes for speed
-  dataFile.print("Speed(kmph): "); dataFile.println(gps.f_speed_kmph());
-  
-  dataFile.println();
-   dataFile.println(); //create a new row to read data more clearly
-    dataFile.close();   //close file
-    Serial.println();   //print to the serial port too:
-}
-else
-  {
+    int year;
+    byte month, day, hour, minute, second, hundredths;
+    gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths);
+    // Print data and time
+    dataFile.print("Date: ");
+    dataFile.print(month, DEC);
+    dataFile.print("/");
+    dataFile.print(day, DEC);
+    dataFile.print("/");
+    dataFile.print(year);
+    int echour = hour - 5;
+    dataFile.print("  Time: ");
+    dataFile.print(echour, DEC);
+    dataFile.print(":");
+    dataFile.print(minute, DEC);
+    dataFile.print(":");
+    dataFile.print(second, DEC);
+    dataFile.print(".");
+    dataFile.println(hundredths, DEC);
+
+    // Here you can print the altitude and course values directly since
+    // there is only one value for the function
+    dataFile.print("Altitude (meters): ");
+    dataFile.println(gps.f_altitude());
+    // Same goes for course
+    dataFile.print("Course (degrees): ");
+    dataFile.println(gps.f_course());
+    // And same goes for speed
+    dataFile.print("Speed(kmph): ");
+    dataFile.println(gps.f_speed_kmph());
+
+    dataFile.println();
+    dataFile.println();  //create a new row to read data more clearly
+    dataFile.close();    //close file
+    Serial.println();    //print to the serial port too:
+  } else {
     Serial.println("error opening datalog.txt");
+  }
 
-  } 
-  
-  
+
   // Same goes for date and time
   int year;
   byte month, day, hour, minute, second, hundredths;
-  gps.crack_datetime(&year,&month,&day,&hour,&minute,&second,&hundredths);
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths);
   // Print data and time
-  Serial.print("Date: "); Serial.print(month, DEC); Serial.print("/"); 
-  Serial.print(day, DEC); Serial.print("/"); Serial.print(year);
+  Serial.print("Date: ");
+  Serial.print(month, DEC);
+  Serial.print("/");
+  Serial.print(day, DEC);
+  Serial.print("/");
+  Serial.print(year);
   int echour = hour - 5;
-  Serial.print("  Time: "); Serial.print(echour, DEC); Serial.print(":"); 
-  Serial.print(minute, DEC); Serial.print(":"); Serial.print(second, DEC); 
-  Serial.print("."); Serial.println(hundredths, DEC);
+  Serial.print("  Time: ");
+  Serial.print(echour, DEC);
+  Serial.print(":");
+  Serial.print(minute, DEC);
+  Serial.print(":");
+  Serial.print(second, DEC);
+  Serial.print(".");
+  Serial.println(hundredths, DEC);
   //Since month, day, hour, minute, second, and hundr
-  
- // BobR Write TIME to Display
-//- OpenLCD.write("                "); // clear display
-//-  OpenLCD.write("                ");
+
+  // BobR Write TIME to Display
+  //- OpenLCD.write("                "); // clear display
+  //-  OpenLCD.write("                ");
 
   //OpenLCD.write(254); // move cursor to beginning of first line
   //OpenLCD.write(192);
   //lcd.setCursor(0,0);
- 
- //- OpenLCD.write("Time: "); OpenLCD.print(hour, DEC); OpenLCD.print(":");
- //- OpenLCD.print(minute, DEC); OpenLCD.print(":"); 
- //- OpenLCD.print(second, DEC); OpenLCD.print(":"); OpenLCD.print(hundredths, DEC);
- 
-// lcd.init();
-lcd.setCursor(0,0);
-lcd.print("Time: ");
-lcd.print(echour, DEC);
-lcd.print(":");
-lcd.print(minute, DEC);
-lcd.print(":");
-lcd.print(second, DEC);
-lcd.print(":");
-lcd.print(hundredths, DEC);
-lcd.print("  ");
 
-//display speed
-lcd.setCursor(0,1);
-lcd.print("Speed: ");
-lcd.print(gps.f_speed_kmph());
-  // Here you can print the altitude and course values directly since 
+  //- OpenLCD.write("Time: "); OpenLCD.print(hour, DEC); OpenLCD.print(":");
+  //- OpenLCD.print(minute, DEC); OpenLCD.print(":");
+  //- OpenLCD.print(second, DEC); OpenLCD.print(":"); OpenLCD.print(hundredths, DEC);
+
+  // lcd.init();
+  lcd.setCursor(0, 0);
+  lcd.print("Time: ");
+  lcd.print(echour, DEC);
+  lcd.print(":");
+  lcd.print(minute, DEC);
+  lcd.print(":");
+  lcd.print(second, DEC);
+  lcd.print(":");
+  lcd.print(hundredths, DEC);
+  lcd.print("  ");
+
+  //display speed
+  lcd.setCursor(0, 1);
+  lcd.print("Speed: ");
+  lcd.print(gps.f_speed_kmph());
+  // Here you can print the altitude and course values directly since
   // there is only one value for the function
-  Serial.print("Altitude (meters): "); Serial.println(gps.f_altitude());  
+  Serial.print("Altitude (meters): ");
+  Serial.println(gps.f_altitude());
   // Same goes for course
-  Serial.print("Course (degrees): "); Serial.println(gps.f_course()); 
+  Serial.print("Course (degrees): ");
+  Serial.println(gps.f_course());
   // And same goes for speed
-  Serial.print("Speed(kmph): "); Serial.println(gps.f_speed_kmph());
+  Serial.print("Speed(kmph): ");
+  Serial.println(gps.f_speed_kmph());
   Serial.println();
-  
+
   // Here you can print statistics on the sentences (not used here).
   // unsigned long chars;
   // unsigned short sentences, failed_checksum;
